cf758bcd14109d896aa9577baa3dbe00bfc1ef28
ag076810 <88880369+ag076810@users.noreply.github.com>
2022-09-05 16:45:34 +0800

    Add files via upload

---
+++ hot-restaurant-master/app/data/table-data.js
@@ -0,0 +1,18 @@
+var table_data = [
+
+	{
+		name: 'Bill',
+		Phone: 5555555,
+		email: 'bill@bill.com',
+		uniqueId: 1	
+	},
+	{
+		name: 'Wendy',
+		Phone: 7777777,
+		email: 'wendy@wendy.com',
+		uniqueId: 2
+	}
+	
+]
+
+module.exports = table_data;
---
+++ hot-restaurant-master/server.js
@@ -0,0 +1,32 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var table_data = require('./app/data/table-data.js');
+var waiting_list = require('./app/data/waitinglist-data.js');
+var fs = require('fs');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+require('/app/routing/html-routes.js')(app);
+
+
+
+require('/app/routing/html-routes.js')(app);
+require('/app/routing/api-routes.js')(app);
+
+// Sets up the Express app to handle data parsing 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+// Starts the server to begin listening 
+// =============================================================
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
+})
